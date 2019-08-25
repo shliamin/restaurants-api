a9b91317bacd06e48e4b1d7160eda83a860a177c
Efim Shliamin <shliamin@icloud.com>
2019-08-25 16:58:26 +0300

    creating api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
+  # [...]
   devise_for :users
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index ]
+    end
+  end
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
